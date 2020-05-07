e63b114de2eaaeb16090aabada8981102cb9fc0d
L1liana <64819049+L1liana@users.noreply.github.com>
2020-05-07 13:05:41 -0600

    Update and rename gpo_paterno_nombre_1-3.js to e_lopez_liliana_1-3.js

---
+++ Secuenciales/e_lopez_liliana_1-3.js
@@ -0,0 +1,6 @@
+let p = require('readline-sync');
+let pi = Math.PI;
+let r= Number(p.question('Ingresa el radio de la base del cono: '));
+let h= Number(p.question('Ingresa la altura del cono: '));
+let volumen = (1/3)*pi* Math.pow(r,2)*h;
+console.log('El volumen del cono es :' +volumen);
--- Secuenciales/gpo_paterno_nombre_1-3.js
+++
@@ -1,7 +0,0 @@
-//Nombre:
-//Grupo:
-//Grado:
-//Fecha:
-//Problema 1.3.	Calcular el volumen de un cono
-let read = require('readline-sync');
-
