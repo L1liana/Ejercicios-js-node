b1869e7cdc76cedddb1b2de8b69ab59a2084c83e
L1liana <64819049+L1liana@users.noreply.github.com>
2020-05-07 13:57:29 -0600

    Update and rename gpo_paterno_nombre_3-3.js to e_lopez_liliana_3-3.js

---
+++ Ciclos/e_lopez_liliana_3-3.js
@@ -0,0 +1,16 @@
+let p = require('readline-sync');
+let ab=0;
+let am=0;
+let as=0;
+let b = Number(p.question('Ingresa la cantidad de billetes: '));
+for(i=1; i<=b; i++){
+  let vb = Number(p.question('Ingresa el valor del billete: '));
+  ab=ab+vb;
+}
+let m = Number(p.question('Ingresa la cantidad de monedas: '));
+for(i=1; i<=m; i++){
+  let vm = Number(p.question('Ingresa el valor del billete: '));
+  am=am+vm;
+}
+as=ab+am;
+console.log('El total de la caja registradora es: '+as)
--- Ciclos/gpo_paterno_nombre_3-3.js
+++
@@ -1,7 +0,0 @@
-//Nombre:
-//Grupo:
-//Grado:
-//Fecha:
-//Problema 3.3.	caja registradora
-let read = require('readline-sync');
-
