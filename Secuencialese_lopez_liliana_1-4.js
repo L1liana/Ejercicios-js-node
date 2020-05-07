a5a8bb62eba2bf04b355c7603595946677d3ff53
L1liana <64819049+L1liana@users.noreply.github.com>
2020-05-07 13:58:32 -0600

    Delete Secuencialese_lopez_liliana_1-4.js

--- Secuencialese_lopez_liliana_1-4.js
+++
@@ -1,5 +0,0 @@
-let p = require('readline-sync');
-let b= Number(p.question('Ingresa la base del triángulo: '));
-let h= Number(p.question('Ingresa la altura altura del triángulo: '));
-let area = (b*h)/2;
-console.log('El área del triángulo es :' +area);
