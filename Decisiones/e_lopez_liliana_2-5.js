2391b3722a3e5111435550199f2c53c20653c011
L1liana <64819049+L1liana@users.noreply.github.com>
2020-05-07 12:27:43 -0600

    Update and rename gpo_paterno_nombre_2-5.js to e_lopez_liliana_2-5.js

---
+++ Decisiones/e_lopez_liliana_2-5.js
@@ -0,0 +1,23 @@
+let p = require('readline-sync');
+let h= Number(p.question('Ingresa la calificación del 1 al 10: '));
+if(h==10){
+console.log("La calificación en letra es: A ")
+}
+else if(h==9){
+console.log("La calificación en letra es: B")
+}
+else if(h==8){
+console.log("La calificación en letra es: C")
+}
+else if(h==7){
+console.log("La calificación en letra es: D")
+}
+else if(h==6){
+console.log("La calificación en letra es: E")
+}
+else if(h=5 && h>=1){
+console.log("La calificación en letra es: F")
+}
+else{
+  console.log("Un numero del 1 al 10")
+}
--- Decisiones/gpo_paterno_nombre_2-5.js
+++
@@ -1,7 +0,0 @@
-//Nombre:
-//Grupo:
-//Grado:
-//Fecha:
-//Problema 2.5.	Calificaciones
-let read = require('readline-sync');
-
