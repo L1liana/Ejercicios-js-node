9e367ad20de62620aa2077a14cc421eb78978374
L1liana <64819049+L1liana@users.noreply.github.com>
2020-05-12 13:52:21 -0600

    Update e_lopez_liliana_1-5.js

--- Secuenciales/e_lopez_liliana_1-5.js
+++ Secuenciales/e_lopez_liliana_1-5.js
@@ -1,3 +1,7 @@
+///Nombre :Liliana Rocio López Jiménez
+/// Grado y grupo: 4°E
+///Secuenciales
+///Ejercicio 1.5
 let p = require('readline-sync');
 let pi = Math.PI;
 let r= Number(p.question('Ingresa el radio: '));
