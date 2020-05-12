5f9da375748b5103e43cee345240a433b7543160
L1liana <64819049+L1liana@users.noreply.github.com>
2020-05-12 13:51:19 -0600

    Update e_lopez_liliana_1-4.js

--- Secuenciales/e_lopez_liliana_1-4.js
+++ Secuenciales/e_lopez_liliana_1-4.js
@@ -1,3 +1,7 @@
+///Nombre :Liliana Rocio López Jiménez
+/// Grado y grupo: 4°E
+///Secuenciales
+///Ejercicio 1.4
 let p = require('readline-sync');
 let b= Number(p.question('Ingresa la base del triángulo: '));
 let h= Number(p.question('Ingresa la altura altura del triángulo: '));
