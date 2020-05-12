f9b7df6c751b18b6296f8821a44d46937a60ad19
L1liana <64819049+L1liana@users.noreply.github.com>
2020-05-12 13:53:47 -0600

    Update e_lopez_liliana_2-1.js

--- Decisiones/e_lopez_liliana_2-1.js
+++ Decisiones/e_lopez_liliana_2-1.js
@@ -1,3 +1,7 @@
+///Nombre: Liliana Rocio López Jiménez
+/// Grado y grupo: 4°E
+///Decisiones
+///Ejercicio 2.1
 let p = require('readline-sync');
 let edad= Number(p.question('Ingresa tu edad: '));
 if(edad>=18){
@@ -6,3 +10,4 @@ if(edad>=18){
 else{
   console.log('No puedes votar por ser menor de edad')
 }
+
