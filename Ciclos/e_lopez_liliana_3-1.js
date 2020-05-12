0f84b03c093e5b0665f9f1fca75e9ce90d68a5f7
L1liana <64819049+L1liana@users.noreply.github.com>
2020-05-12 14:06:13 -0600

    Update e_lopez_liliana_3-1.js

--- Ciclos/e_lopez_liliana_3-1.js
+++ Ciclos/e_lopez_liliana_3-1.js
@@ -1,10 +1,15 @@
+///Nombre: Liliana Rocio López Jiménez
+/// Grado y grupo: 4°E
+///Ciclos
+///Ejercicio 3.1
 let p = require('readline-sync');
 let s= Number(1500);
 let a=0;
-for(i=0; i<=6; i++){
+for(i=1; i<=6; i++){
  aum= s*.10;
  s=s+aum;
  a=a+s;
- console.log("El sueldo anual es: "+s)
+ console.log("El sueldo anual del año "+i+" es: "+s);
 }
-console.log("Sueldo final: "+a)
+console.log("Sueldo final: "+a);
+
