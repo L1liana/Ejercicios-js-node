ab2ea2fc618d8f29745c28c53425fd0c26a56e1b
L1liana <64819049+L1liana@users.noreply.github.com>
2020-05-12 13:15:41 -0600

    Update e_lopez_liliana_1-1.js

--- Secuenciales/e_lopez_liliana_1-1.js
+++ Secuenciales/e_lopez_liliana_1-1.js
@@ -1,3 +1,7 @@
+///Nombre :Liliana Rocio López Jiménez
+/// Grado y grupo: 4°E
+///Secuenciales
+///Ejercicio 1.1
 let p = require('readline-sync');
 let h= Number(p.question('Ingresa la altura del trapecio: '));
 let l1 = Number(p.question('Ingresa el lado paralelo 1 del trapecio: '));
