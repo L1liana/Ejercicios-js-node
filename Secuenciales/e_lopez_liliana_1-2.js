942ecf87ce5497eed97b75b6d03a15053a5c9845
L1liana <64819049+L1liana@users.noreply.github.com>
2020-05-12 13:33:00 -0600

    Update e_lopez_liliana_1-2.js

--- Secuenciales/e_lopez_liliana_1-2.js
+++ Secuenciales/e_lopez_liliana_1-2.js
@@ -1,3 +1,7 @@
+///Nombre :Liliana Rocio López Jiménez
+/// Grado y grupo: 4°E
+///Secuenciales
+///Ejercicio 1.2
 let p = require('readline-sync');
 let l= Number(p.question('Ingresa el lado del cuadrado: '));
 let apotema = l/2;
